eb766d8e5c914fa8f2b8fe1bcd05df1cb3834403
Steve-Kim <pnlglobaltra@gmail.com>
2023-03-12 03:41:16 +1100

    17th commit

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const io = new Server(server, {
      cors: {
         //  origin: "http://localhost:3000",
         //  origin: "http://localhost:3001",
-        origin: ['http://localhost:1234', 'https://n-g2y6lzhpbp3kuko4s2uwm42vxxdu6hd4ckcoova-0lu-script.googleusercontent.com'],
+        origin: ['http://localhost:1234', '*'],
         // origin:  "https://n-g2y6lzhpbp3kuko4s2uwm42vxxdu6hd4ckcoova-0lu-script.googleusercontent.com",
         methods: ["GET","POST"],
      }
